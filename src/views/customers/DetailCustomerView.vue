d3cfe867a7ba06a861340c66467a5e1a905df379
alfredamos <alfredamos@gmail.com>
2023-04-21 02:19:15 +0100

    add detail functionality to customer, genre, movie and rental

--- src/views/customers/DetailCustomerView.vue
+++ src/views/customers/DetailCustomerView.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import SingleCustomer from "./SingleCustomer.vue";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import apiCustomer from "../../services/api-customer.service";
+import type CustomerDto from "../../components/models/customers/customer.model";
+
+const { id } = useRoute().params;
+const customer = ref<CustomerDto>(null!);
+onMounted(() => {
+  apiCustomer
+    .findOne(+id)
+    .then((resp) => {
+      customer.value = resp.data
+    })
+    .catch((err) => console.log("error : ", err.message));
+});
+</script>
+
 <template>
-      <div>Detail Customer</div>
+  <SingleCustomer v-if="customer" :customer="customer" />
 </template>
---
+++ src/views/customers/SingleCustomer.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type CustomerDto from "../../components/models/customers/customer.model";
+
+interface Props {
+  customer: CustomerDto;
+}
+const props = defineProps<Props>();
+</script>
+<template>
+  <div class="border pado">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="text-center">Customer Detail</h4>
+      </div>
+      <div class="card-body">
+        <ul class="list-group">
+          <li class="list-group-item">
+            Name: &nbsp; <b>{{ props.customer.name }}</b>
+          </li>
+          <li class="list-group-item">
+            Email: &nbsp; <b>{{ props.customer.email }}</b>
+          </li>
+          <li class="list-group-item">
+            Phone: &nbsp; <b>{{ props.customer.phone }}</b>
+          </li>
+          <li class="list-group-item">
+            Gold: &nbsp; <b>{{ props.customer.isGold ? "Yes" : "No" }}</b>
+          </li>
+        </ul>
+      </div>
+      <div class="card-footer">
+        <RouterLink
+          class="btn btn-outline-secondary form-control fw-bold"
+          to="/customers"
+          >Back</RouterLink
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.pado {
+  padding: 10px;
+}
+</style>
